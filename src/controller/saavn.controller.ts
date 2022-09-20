47afa53fdd33dcda240e00e45f537cdd3c518ea4
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-20 13:44:36 +0530

    Update saavn.controller.ts

--- src/controller/saavn.controller.ts
+++ src/controller/saavn.controller.ts
@@ -123,7 +123,10 @@ export class Controller {
 
       const songDetails = await SongService.songDetails(identifier)
 
-      res.json({ status: globalConstants.status.success, results: songDetails })
+      res.json()
+      
+      //res.json({ status: globalConstants.status.success, results: songLyrics })
+
     } catch (error) {
       next(error)
     }
