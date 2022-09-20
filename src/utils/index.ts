5218c67db82e8062a8ef2b663e2bc1a778ec8601
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-21 02:59:11 +0530

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,7 +12,7 @@ export class Utils {
       ]
 
     return (
-      qualities.map((quality) => 
+      qualities((quality) => 
         
         link: link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_160'),
       ) || false
