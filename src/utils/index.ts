911f9134c1afed21155b08ce9e8b4405837dd17f
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-21 03:53:55 +0530

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,13 +6,19 @@ export class Utils {
   // create download links for different bitrates
   public static createDownloadLinks = (link: string) => {
     if (!link) return false
-    return (
-           
 
-       
+    const qualities = [
+      
+            { id: '_160', bitrate: '160kbps' },
+
+      
+    ]
+
+    return (
+      qualities.map((quality) => 
        link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_160'),
       ) || false
-    
+    )
   }
 
   // create image links for different resolutions
