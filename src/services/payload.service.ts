61574fcdf6ae8f4d7c60fea1f187fd14fd217ce1
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-20 17:33:43 +0530

    Update payload.service.ts

--- src/services/payload.service.ts
+++ src/services/payload.service.ts
@@ -23,7 +23,7 @@ export class GeneratePayload {
       image: Utils.createImageLinks(song.image),
       url: song.perma_url,
       copyright: song.copyright_text,
-      media_url: Utils.createDownloadLinks,
+      media_url: Utils.createDownloadLinks(song.media_preview_url),
     }
     return songPayload
   }
