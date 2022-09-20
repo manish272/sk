5e89d8c3833295f275ffe927272e806dcfe9ce82
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-20 19:06:55 +0530

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ type IdentifierType = 'song' | 'album'
 
 export class Utils {
   // create download links for different bitrates
-  public static createDownloadLinks = (link) => {
+  public static createDownloadLinks = (link: string) => {
     if (!link) return false
 
     const qualities = [
