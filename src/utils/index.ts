b457e2303a05296d4a4b7804220edff2ae3d052e
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-20 17:46:00 +0530

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,13 +7,15 @@ export class Utils {
   public static createDownloadLinks = (link: string) => {
     if (!link) return false
 
-    
+    const qualities = [
+      { id: '_160', bitrate: '160kbps' },
+      ]
 
     return (
-      
+      qualities.map((quality) => ({
         
-        link: link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_160'),
-       || false
+        link: link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', quality.id),
+      })) || false
     )
   }
 
