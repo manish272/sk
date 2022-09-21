e06d782b0093cfb04289d182cc48f57dcb037800
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-21 12:49:12 +0530

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,15 +8,16 @@ export class Utils {
     if (!link) return false
 
     const qualities = [
-      
-            { id: '_160', bitrate: '160kbps' },
-
-      
+      { id: '_12', bitrate: '12kbps' },
+      { id: '_48', bitrate: '48kbps' },
+      { id: '_96', bitrate: '96kbps' },
+      { id: '_160', bitrate: '160kbps' },
+      { id: '_320', bitrate: '320kbps' },
     ]
 
     return (
       qualities.map((quality) => 
-       link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_160'),
+       link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', quality.id),
       ) || false
     )
   }
